a82891cfd585cd59b729a4aded63ea0ce1823d9b
StageGuard <sg@mamoe.net>
2024-12-27 11:27:08 +0800

    install build tools

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 brew update
-yes | brew install ffmpeg libplacebo zlib libass lua luajit libjpeg \
+yes | brew install meson ninja ffmpeg libplacebo zlib libass lua luajit libjpeg \
     uchardet vulkan-loader vulkan-headers zimg zmq libxcb webp \
     dav1d freetype2 fribidi harfbuzz libunibreak openssl mujs \
     opus rav1e rubberband sdl2 shaderc snappy libsodium libsoxr \
